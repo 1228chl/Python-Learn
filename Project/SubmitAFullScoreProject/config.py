42e242dc7e40094ef9a628cb467222319d7b8d9b
XunZong <2124159894@qq.com>
2026-06-13 12:07:05 +0800

    NLP 投满分项目day01

--- Project/SubmitAFullScoreProject/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    def __init__(self):
-        self.root_path = r'\Project\SubmitAFullScoreProject'
-        self.train_path = self.root_path + r'\data\train.txt'
-        self.dev_path = self.root_path + r'\data\dev.txt'
-        self.test_path = self.root_path + r'\data\test.txt'
